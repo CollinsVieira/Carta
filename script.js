4c06bd5732994e848512e89b2660add52715cdbe
CollinsVieira <collins000000000@gmail.com>
2024-02-12 12:15:23 -0500

    mousecora

--- script.js
+++ script.js
@@ -18,4 +18,25 @@ $(document).ready(function () {
       });
   });
 
+
+
+  document.addEventListener('mousemove',function(e){
+    let body = document.querySelector('body');
+    let cora = document.createElement('span');
+    let x = e.offsetX;
+    let y = e.offsetY;
+    cora.style.left = x+'px';
+    cora.style.top = y+'px';
+
+    let size = Math.random() * 80;
+    cora.style.width = 20 + size + 'px';
+    cora.style.height = 20 + size + 'px';
+
+    let transformValue = Math.random() * 360;
+    cora.style.trasnform = 'rotate('+ transformValue +'deg)'
+
+    body.appendChild(cora)
+
+    setTimeout(function(){cora.remove();},1000)
+  })
   
